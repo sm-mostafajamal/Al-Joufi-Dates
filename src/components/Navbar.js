e856df12c78505df41cbaaaa59981f8fe39e5e3c
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-10-25 07:29:28 +0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Products</a></li>
+        <li><a href="">Gallery</a></li>
+        <li><a href="">About us</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
